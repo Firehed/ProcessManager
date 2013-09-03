97a9918d8b4bbe327d35c5467eda21baee4bfc86
Eric Stern <eric@ericstern.com>
2013-09-03 14:04:27 -0700

    overhaul logging to PSR spec

--- GearmanProcessManager.php
+++ GearmanProcessManager.php
@@ -14,7 +14,7 @@ class GearmanProcessManager extends Firehed\ProcessControl\ProcessManager {
 	protected function doWork() {
 		$worker = $this->getWorker();
 		if ($worker->work()) {
-			$this->logDebug("$this->myPid processed a job");
+			$this->getLogger()->debug("$this->myPid processed a job");
 			$this->reconnects = 0;
 			return true;
 		}
@@ -22,14 +22,14 @@ class GearmanProcessManager extends Firehed\ProcessControl\ProcessManager {
 			case GEARMAN_IO_WAIT:
 			case GEARMAN_NO_JOBS:
 				if (@$worker->wait()) {
-					$this->logDebug("$this->myPid waited with no error");
+					$this->getLogger()->debug("$this->myPid waited with no error");
 					$this->reconnects = 0;
 					return true;
 				}
 				if ($worker->returnCode() == GEARMAN_NO_ACTIVE_FDS) {
-					$this->logError("$this->myPid Connection to gearmand server failed");
+					$this->getLogger()->error("$this->myPid Connection to gearmand server failed");
 					if (++$this->reconnects >= 5) {
-						$this->logError("$this->myPid Giving up");
+						$this->getLogger()->error("$this->myPid Giving up");
 						$this->stopWorking();
 					}
 					else {
@@ -38,14 +38,14 @@ class GearmanProcessManager extends Firehed\ProcessControl\ProcessManager {
 				}
 				break;
 			default:
-				$this->logError("$this->myPid exiting after getting code {$worker->returnCode()}");
+				$this->getLogger()->error("$this->myPid exiting after getting code {$worker->returnCode()}");
 				$this->stopWorking();
 		}
 	}
 
 	private function getWorker() {
 		if (!$this->worker) {
-			$this->logDebug("Building new worker");
+			$this->getLogger()->debug("Building new worker");
 			$this->worker = new GearmanWorker();
 			$this->worker->addOptions(GEARMAN_WORKER_NON_BLOCKING);
 			$this->worker->setTimeout(2500);
--- src/Firehed/ProcessControl/ProcessManager.php
+++ src/Firehed/ProcessControl/ProcessManager.php
@@ -14,10 +14,19 @@ abstract class ProcessManager {
 
 	protected $myPid;
 
-	public function __construct() {
+	public function __construct(\Psr\Log\LoggerInterface $logger = null) {
 		$this->managerPid = $this->myPid = getmypid();
-		$this->setLogger(new \Psr\Log\NullLogger);
+		if ($logger) {
+			$this->setLogger($logger);
+		}
+		else {
+			$this->setLogger(new \Psr\Log\NullLogger);
+		}
 		$this->installSignals();
+	}
+
+	protected function getLogger() {
+		return $this->logger;
 	}
 
 	public function setWorkerCount($count) {
@@ -38,35 +47,20 @@ abstract class ProcessManager {
 		$status = null;
 		if ($exited = pcntl_wait($status, WNOHANG)) {
 			unset($this->workerProcesses[$exited]);
-			$this->logInfo("Worker $exited got WNOHANG during normal operation");
+			$this->getLogger()->info("Worker $exited got WNOHANG during normal operation");
 		}
 	}
 
 	abstract protected function doWork();
 
 	private function installSignals() {
-		$this->logDebug("$this->myPid SIGTERM handler installation");
+		$this->getLogger()->debug("$this->myPid SIGTERM handler installation");
 		pcntl_signal(SIGTERM, [$this,'signal']);
 		pcntl_signal(SIGINT,  [$this,'signal']);
 	}
 
 	private function isParent() {
 		return $this->myPid == $this->managerPid;
-	}
-
-	protected function logDebug($str) {
-		// $this->logInfo($str);
-		$this->logger->debug($str);
-	}
-
-	protected function logError($str) {
-		// echo "$str\n";
-		$this->logger->error($str);
-	}
-
-	protected function logInfo($str) {
-		// $this->logError($str);
-		$this->logger->info($str);
 	}
 
 	private function manageWorkers() {
@@ -85,42 +79,42 @@ abstract class ProcessManager {
 	public function signal($signo) {
 		if ($this->isParent()) {
 			$this->stopWorking();
-			$this->logInfo('Parent got sigterm');
-			$this->logDebug("Children: " . print_r($this->workerProcesses, true));
+			$this->getLogger()->info('Parent got sigterm');
+			$this->getLogger()->debug("Children: " . print_r($this->workerProcesses, true));
 			$this->stopChildren(SIGTERM);
 			while ($this->workerProcesses) {
 				$status = null;
 				if ($exited = pcntl_wait($status, WNOHANG)) {
 					unset($this->workerProcesses[$exited]);
-					$this->logDebug("Worker $exited got WNOHANG");
+					$this->getLogger()->debug("Worker $exited got WNOHANG");
 				}
 				else {
 					sleep(1);
 				}
 			}
-			$this->logInfo("Parent shutting down");
+			$this->getLogger()->info("Parent shutting down");
 			exit;
 		}
 		else {
-			$this->logInfo("Child $this->myPid received SIGTERM; stopping work");
+			$this->getLogger()->info("Child $this->myPid received SIGTERM; stopping work");
 			$this->stopWorking();
 		}
 	}
 
 	private function spawnWorker() {
-		$this->logInfo("Creating a new worker");
+		$this->getLogger()->info("Creating a new worker");
 		switch ($pid = pcntl_fork()) {
 		case -1: // Failed
-			$this->logError("Spawning worker failed");
+			$this->getLogger()->error("Spawning worker failed");
 			exit(2);
 		case 0:  // Child
 			$this->myPid = getmypid();
-			$this->logInfo("$this->myPid created");
+			$this->getLogger()->info("$this->myPid created");
 			$this->installSignals();
 			$this->work();
 			break;
 		default: // Parent
-			$this->logDebug("Parent created child with pid $pid");
+			$this->getLogger()->debug("Parent created child with pid $pid");
 			$this->workerProcesses[$pid] = $pid;
 			break;
 		}
@@ -128,10 +122,10 @@ abstract class ProcessManager {
 
 	private function stopChildren($sig = SIGTERM) {
 		foreach ($this->workerProcesses as $pid) {
-			$this->logDebug("Sending SIGTERM to $pid");
+			$this->getLogger()->debug("Sending SIGTERM to $pid");
 			posix_kill($pid, $sig);
 			if (!posix_kill($pid, 0)) {
-				$this->logDebug("$pid is dead already");
+				$this->getLogger()->debug("$pid is dead already");
 			}
 		}
 	}
@@ -141,13 +135,13 @@ abstract class ProcessManager {
 	}
 
 	private function work() {
-		$this->logDebug("Child $this->myPid about to start work");
+		$this->getLogger()->debug("Child $this->myPid about to start work");
 		while ( $this->shouldWork ) {
 			$_SERVER['REQUEST_TIME'] = time();
 			$_SERVER['REQUEST_TIME_FLOAT'] = microtime(true);
 			$this->doWork();
 		}
-		$this->logInfo("Child $this->myPid exiting");
+		$this->getLogger()->info("Child $this->myPid exiting");
 		exit;
 	}
 
