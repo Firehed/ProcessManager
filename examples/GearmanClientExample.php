7a1c0fd8897d2472540d917eea62a49fb6e9e94f
Eric Stern <eric@ericstern.com>
2013-09-03 14:41:47 -0700

    Make examples much more detailed

--- examples/GearmanClientExample.php
+++ examples/GearmanClientExample.php
@@ -1,7 +1,19 @@
 <?php
-$client= new GearmanClient();
-$client->addServer();
-print $client->do("reverse", "Hello World!");
 
-print $client->do('caps', 'some lowercase string') . "\n";
+// This is an example of how to submit a job to Gearman for background 
+// processing. It's not specific to GearmanWorkerExample, but it does use the 
+// functions that example registers. Run this *after* starting the worker.
+
+$client = new GearmanClient();
+$client->addServer();
+
+echo "Sending a 'flip_it' job to gearman...\n";
+echo "Returned: ";
+echo $client->doNormal("flip_it", "Hello World!");
+echo "\n";
+
+echo "Sending a 'my_uppercase' job to gearman...\n";
+echo "Returned: ";
+echo $client->doNormal('my_uppercase', 'some lowercase string');
+echo "\n";
 
--- examples/GearmanWorkerExample.php
+++ examples/GearmanWorkerExample.php
@@ -1,29 +1,49 @@
 <?php
 
+// Use Composer's autoloader
 if (file_exists('./vendor/autoload.php')) {
 	require_once './vendor/autoload.php';
 } elseif (file_exists('../../../vendor/autoload.php')) {
 	require_once '../../../vendor/autoload.php';
 }
 
-declare(ticks=1);
-Firehed\ProcessControl\Daemon::run();
-
-function my_reverse_function($job) {
+// Wrapper for strrev(), invoked when a "flip_it" job is received (more below)
+function my_reverse_function(GearmanJob $job) {
 	return strrev($job->workload());
 }
 
+// Required example code: set up ticks (or process control signals can't 
+// register)
+declare(ticks=1);
+
+// Optional but highly recommended: daemonize the worker manager
+Firehed\ProcessControl\Daemon::run();
+
+// Get a new worker manager for configuration
 $pm = new Firehed\ProcessControl\GearmanWorkerManager;
 
-// Example GearmanProcessManager class jobs
-$pm->addFunction("my_reverse_function", "my_reverse_function");
+// Example GearmanWorkerManager class jobs
+//
+// Param 1 is the job name (corresponds to param 1 of GearmanClient::doX class 
+// of functions)
+//
+// Param 2 is the callable to invoke when such a job is received - anything 
+// callable with "call_user_func" should work. The function must accept one 
+// parameter: GearmanJob $job. This means native PHP funtions must be wrapped: 
+// see 'my_reverse_function'
+//
+
+$pm->addFunction("flip_it", "my_reverse_function");
 
 $pm->addFunction("my_uppercase", function(GearmanJob $job) {
 	return strtoupper($job->workload());
 });
 
-// ProcessManager requirement
+// Optional: set number of workers
+$pm->setWorkerCount(2);
+
+// When other configuration options are available, examples will be added here
+
+// ProcessManager requirement: start working
 $pm->start();
 
-
-// Client: $client->doBackground('my_uppercase', $some_string_to_uppercase);
